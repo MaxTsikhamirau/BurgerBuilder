c71ed94811ebb03b7bfa9b671810b096b5af534f
HGRUPP\maksim.tsikhamirau <maksim.tsikhamirau@helmes.ee>
2018-05-24 06:36:13 +0300

    Base component snippets+Aux

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Aux from '../../hoc/Aux';
+import Aux from '../../hoc/Auxis';
 import classes from './Layout.css';
 
 const layout = (props) => {
---
+++ src/hoc/Auxis.js
@@ -0,0 +1,3 @@
+const aux = (props) => {
+    return (<div>{props.children}</div>)
+}
